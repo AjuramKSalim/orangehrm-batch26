30c1ad9e6939e7d515f9b25cdf0a46e345d953b1
Anjana <anjanajohny123@gmail.com>
2022-11-30 17:07:09 -0500

    committed changes

--- orangehrm-batch26/src/test/java/ca/qaguru/orangehrmbatch26/pages/LicensePage.java
+++ orangehrm-batch26/src/test/java/ca/qaguru/orangehrmbatch26/pages/LicensePage.java
@@ -3,21 +3,41 @@ package ca.qaguru.orangehrmbatch26.pages;
 import ca.qaguru.orangehrmbatch26.library.PageBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
 
 public class LicensePage extends PageBase {
 
-    private final String licenseAdd =".oxd-button--secondary" ;
-    private final String licenseName="div[class='oxd-form-row'] input[class*='oxd-input']";
-    private final String licenseSave="button[class*='oxd-button--secondary']";
+    private final String licenseAdd = ".oxd-button--secondary";
+    private final String licenseName = "div[class='oxd-form-row'] input[class*='oxd-input']";
+    private final String licenseSave = "button[class*='oxd-button--secondary']";
 
-    public LicensePage(WebDriver driver) {super(driver);
+    //private  final  String licenseCancel=".oxd-form-actions button[type='button']";
+
+
+    public LicensePage(WebDriver driver) {
+        super(driver);
     }
-
-    public void addNewLicense(String newLicense)
-    {
+    String newLicense;
+    public void licenseNewAdd(String newLicense) {
         click(By.cssSelector(licenseAdd));
         setText(By.cssSelector(licenseName), newLicense);
         click(By.cssSelector(licenseSave));
 
     }
-}
+
+    public void newLicenseVerify() {
+       isElementVisible(By.className("orangehrm-container"));
+
+        List<WebElement> values = driver.findElements(By.xpath("//div[@class='oxd-table-body'] /div /div /div[2] /div"));
+
+        for (WebElement value : values) {
+
+            System.out.println(value.getText());
+            }
+        }
+    }
+
+
--- orangehrm-batch26/src/test/java/ca/qaguru/orangehrmbatch26/tests/LicenseTest.java
+++ orangehrm-batch26/src/test/java/ca/qaguru/orangehrmbatch26/tests/LicenseTest.java
@@ -5,21 +5,30 @@ import ca.qaguru.orangehrmbatch26.pages.HeaderPage;
 import ca.qaguru.orangehrmbatch26.pages.LicensePage;
 import ca.qaguru.orangehrmbatch26.pages.LoginPage;
 import ca.qaguru.orangehrmbatch26.pages.MenuOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
 public class LicenseTest extends TestBase {
 
     @Test
-    public void addNewLicense()
-    {
+    public void addNewLicense() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("admin", "admin123", true, null);
         HeaderPage headerPage = new HeaderPage(driver);
         headerPage.selectMenu(MenuOptions.LICENSES);
-        LicensePage licensePage=new LicensePage(driver);
-        licensePage.addNewLicense("New License Added");
+        LicensePage licensePage = new LicensePage(driver);
+       licensePage.licenseNewAdd("Johny");
+       licensePage.newLicenseVerify();
 
 
+
+       }
 
     }
-}
+
+
