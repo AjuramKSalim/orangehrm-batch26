2026ce3fc592d3cda517a23fb7814462cf7e3168
Anjana <anjanajohny123@gmail.com>
2022-12-04 12:14:48 -0500

    Modified LicensePage

--- orangehrm-batch26/src/test/java/ca/qaguru/orangehrmbatch26/tests/LicenseTest.java
+++ orangehrm-batch26/src/test/java/ca/qaguru/orangehrmbatch26/tests/LicenseTest.java
@@ -1,4 +1,5 @@
 package ca.qaguru.orangehrmbatch26.tests;
+
 import ca.qaguru.orangehrmbatch26.library.TestBase;
 import ca.qaguru.orangehrmbatch26.pages.HeaderPage;
 import ca.qaguru.orangehrmbatch26.pages.LicensePage;
@@ -9,15 +10,14 @@ import org.testng.annotations.Test;
 
 public class LicenseTest extends TestBase {
     @Test
-    public void newLicenseAddition() throws InterruptedException {
-        String License="Anjana License New";
+    public void newLicenseAddition() {
+        String License = "CompTIA A+ Certification";
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("admin", "admin123", true, null);
-        HeaderPage headerPage=new HeaderPage(driver);
+        HeaderPage headerPage = new HeaderPage(driver);
         headerPage.selectMenu(MenuOptions.LICENSES);
-        LicensePage licensePage=new LicensePage(driver);
+        LicensePage licensePage = new LicensePage(driver);
         licensePage.licenseAdd(License);
-        Thread.sleep(3000);
         licensePage.licenseVerify(License);
 
 
