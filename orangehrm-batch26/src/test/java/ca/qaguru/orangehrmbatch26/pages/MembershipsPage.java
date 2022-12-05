5551605f75a8e1e8bff98961cc5203f21a56f028
arya <aryasasok@gmail.com>
2022-12-04 21:55:51 -0500

    created a Membership test page

---
+++ orangehrm-batch26/src/test/java/ca/qaguru/orangehrmbatch26/pages/MembershipsPage.java
@@ -0,0 +1,10 @@
+package ca.qaguru.orangehrmbatch26.pages;
+
+import ca.qaguru.orangehrmbatch26.library.PageBase;
+import org.openqa.selenium.WebDriver;
+
+public class MembershipsPage extends PageBase {
+    public MembershipsPage(WebDriver driver) {
+        super(driver);
+    }
+}
